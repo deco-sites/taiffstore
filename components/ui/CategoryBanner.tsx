9c46983dd9d27e86780021f0381ba7c4b65e4b0f
vmedeirossocial <111756342+vmedeirossocial@users.noreply.github.com>
2024-08-02 15:21:23 -0300

    Update CategoryBanner.tsx

--- components/ui/CategoryBanner.tsx
+++ components/ui/CategoryBanner.tsx
@@ -69,7 +69,6 @@ function Banner(props: SectionProps<ReturnType<typeof loader>>) {
   }
 
   const { image, content, matcher, subcategories } = banner;
-  console.log('subcategories -------------', subcategories)
   return (
     <div class="grid grid-cols-1 grid-rows-1 relative">
       <Picture preload class="col-start-1 col-span-1 row-start-1 row-span-1">
